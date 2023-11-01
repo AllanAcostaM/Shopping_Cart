03404431fe9c824cc62566140e7993e37892a94b
Joseph Acosta <allanacost.27@gmail.com>
2023-11-01 11:15:48 -0500

    Redux: Estilos del componente NavBar y acceso al Store atraves de useSelector

--- Shopping_Cart/src/components/navbar/NavBar.jsx
+++ Shopping_Cart/src/components/navbar/NavBar.jsx
@@ -1,12 +1,16 @@
 import { BsCart4 } from "react-icons/bs";
+import { useSelector } from "react-redux";
 
 const NavBar = () => {
+  // Obtiene un valor del state
+  const { amount } = useSelector((store) => store.cart);
+
   return (
     <header>
       <h1>My store</h1>
       <div className="amount_container">
         <BsCart4 className="cart_icon" />
-        <span className="badge">4</span>
+        <span className="badge">{amount}</span>
       </div>
     </header>
   );
