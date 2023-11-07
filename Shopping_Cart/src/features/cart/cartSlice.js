ca9b89238b6883cdfc5292ce0b1c7b65e4a639a7
Joseph Acosta <allanacost.27@gmail.com>
2023-11-06 22:37:48 -0500

    Redux: Se crea el objeto inicializador del state en un archivo separado

--- Shopping_Cart/src/features/cart/cartSlice.js
+++ Shopping_Cart/src/features/cart/cartSlice.js
@@ -1,36 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
+import cartItems from "../../data/cartData";
 
 const initialState = {
-  cartItems: [
-    {
-      id: "1",
-      title: "Apple iPhone 15 Pro Max",
-      price: 20,
-      img: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-15-pro-max.jpg",
-      amount: 1,
-    },
-    {
-      id: "2",
-      title: "Google Pixel 8 Pro",
-      price: 35,
-      img: "https://fdn2.gsmarena.com/vv/bigpic/google-pixel-8-pro.jpg",
-      amount: 1,
-    },
-    {
-      id: "3",
-      title: "Samsung Galaxy S23 Ultra",
-      price: 60,
-      img: "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-s23-ultra-5g.jpg",
-      amount: 1,
-    },
-    {
-      id: "4",
-      title: "Xiaomi Redmi Note 12 Pro",
-      price: 100,
-      img: "https://fdn2.gsmarena.com/vv/bigpic/xiaomi-redmi-note-12-pro.jpg",
-      amount: 1,
-    },
-  ],
+  cartItems,
   amount: 4,
   total: 1000,
 };
